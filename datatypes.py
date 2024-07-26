f287f1aa2d13140b438a85a40108b25a01a004a1
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-07-26 15:38:20 +0530

    final commit

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+a = 1; # this is a integer
+
+b = 7.22; # this is floating point number
+
+c = "decode"; # this is a string
+
+d = False; # this is a boolean variable
+
+
+
+
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,27 @@
+a = 1;
+b = 2;
+print(a + b);
+
+c = "decode";
+print(c);
+
+#variables can be alphabets, numbers but can't be a special character.
+
+hello = 8; # valid syntax for variable declaration
+
+# @hello = 5  invalid syntax
+
+# arithmetic operators
+g = 6;
+k = 5;
+d = (g + k);
+print(d);
+
+# comparsion operators always return boolean values.
+
+l = 5 == 5;
+print(l);
+
+            
+
+
