dcf927c41468074a0e8b40c729eb6883a9bf258f
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-12-20 23:30:04 +0530

    final commit

--- input.py
+++ input.py
@@ -13,3 +13,9 @@ b = int(input("Enter number 2: "))
 print("Number a is: ", a)
 print("Number b is: ", b) 
 print("Sum is ", a + b)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
